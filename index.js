bcfaf997d28f728eea01fe98f4c17a8f6f0fec37
Ahmed_Agha__ <115808351+mangawy141@users.noreply.github.com>
2024-04-15 13:38:49 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+let mobile_nav = document.querySelector(".primary-navigation");
+let toggle_nav = document.querySelector(".mobile-nav-toggle");
+let clicked = false;
 
+toggle_nav.addEventListener("click", function () {
+  if (clicked) {
+    toggle_nav.classList.toggle("icon-change");
+    mobile_nav.classList.toggle("slide");
+    clicked = false;
+  } else {
+    toggle_nav.classList.toggle("icon-change");
+    mobile_nav.classList.toggle("slide");
+
+    clicked = true;
+  }
+});
+
+document.body.addEventListener("click", function (event) {
+  if (
+    clicked &&
+    !mobile_nav.contains(event.target) &&
+    !toggle_nav.contains(event.target)
+  ) {
+    toggle_nav.classList.toggle("icon-change");
+    mobile_nav.classList.toggle("slide");
+    clicked = false;
+  }
+});
